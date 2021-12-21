7393b37adba6c102328416b9776b6cad63c34e99
haianh1999 <hoanghaianh150699@gmail.com>
2021-12-21 13:49:34 +0700

    Create_Table_SP_HTHA883

---
+++ LTQL_1721050883/Models/HTHASanPham883.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050883.Models
+{
+    public class HTHASanPham883
+    {
+        [Key]
+        [Display(Name = "Mã Sản Phẩm")]
+        [StringLength(20)]
+        public string MaSanPham { get; set; }
+
+        [Display(Name = "Tên Sản Phẩm")]
+        [StringLength(50)]
+        public string TenSanPham { get; set; }
+
+        [Display(Name = "Mã Nhà Cung Cấp")]
+        public int MaNhaCungCap { get; set; }
+    }
+}
--- LTQL_1721050883/Models/NhaCungCap883.cs
+++ LTQL_1721050883/Models/NhaCungCap883.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace LTQL_1721050883.Models
 {
-    public class NhaCungCap
+    public class NhaCungCap883
     {
+        [Key]
+        [Display(Name = "Mã Nhà Cung Cấp")]
+        public int MaNhaCungCap { get; set; }
+
+
+        [Display(Name = "Tên Nhà Cung Cấp")]
+        [StringLength(50)]
+        public string TenNhaCungCap { get; set; }
     }
 }
